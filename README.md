0c58af271420421fce6ab1a1929d8d1b4a542e4d
Maverick Peppers <maverick@Mavericks-iMac.local>
2016-09-11 16:33:49 -0500

    Many new features added. See readme. v0.0.2

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # PlayBackJS
 > Given a spritesheet url or Image object, playback an animation
 
+v0.0.2
+
 # Features
 - Play, pause, and stop supported
 - Supply frame sequence as an array
 - Control playback delay
+- utility functions for mapping cursor position to frames
 - Stand alone javascript plugin - does not need jQuery to work
+- **View the source for more features**
 
 ## Usage
 - Download and install by dropping the ```/PlayBackJS``` in your project's javascript directory
-- Include the script in your HTML headers e.g. ```<script src="js/PlayBackJS/playback.js"></script>```
+- Include the script in your HTML headers e.g.
+
+  ```<script src="js/PlayBackJS/playback.js"></script>```
 - Call the constructor
 ```javascript
   var playback = new PlayBack({
@@ -26,9 +32,44 @@
 Optionally bind actions to control buttons:
 
 ```javascript
-  $('#btnPlaybackPlay').on('click',  () => playback.play());
-  $('#btnPlaybackPause').on('click', () => playback.pause());
-  $('#btnPlaybackStop').on('click',  () => playback.stop());
+$('#btnPlaybackPlay').on('click',  () => playback.play());
+$('#btnPlaybackPause').on('click', () => playback.pause());
+$('#btnPlaybackStop').on('click',  () => playback.stop());
+```
+
+## Events
+With jQuery included, a custom ```draw``` event will emit from the target canvas
+referenced by the required ```target``` options property in the constructor.
+
+```javascript
+  var playback = new PlayBack({..., target: "playbackCanvas", ...});
+  playback.play();
+
+  ...
+
+  // Example: display the animation's current frame on the web page
+  $("#playbackCanvas").on('draw', (e) => { $("#frameCounterLabel").html(e.frame); });
+```
+
+Alternatively, if jQuery is not supplied, use the custom subscribe event function
+found in the ```utils``` class utility object to catch all published draw events.
+
+```javascript
+  var playback = new PlayBack({..., target: "playbackCanvas", ...});
+  playback.play();
+
+  ...
+
+  playback.utils.subscribeToDrawEvent((frame) => { $("#frameCounterLabel").html(frame); });
+```
+
+## Redraw
+When changing properties to the playback object, you'll need to update the target
+canvas render to reflect those new changes. For instance, changing the desired
+animation frames does not automatically force a redraw. Do so by simply calling
+
+```
+playback.refresh();
 ```
 
 ## Contribute
--- playback.js
+++ playback.js
@@ -25,7 +25,7 @@ Options:
 Functions:
   getDelay() - Get the delay in Milliseconds
   setDelay() - Instantly set the delay in Milliseconds
-  getCurrentCell() - Get the current cell
+  getCurrentCell() - Get the current cell aka frame
   getCellCount()   - Get the total number of cells aka frames in the sprtiesheet
   play()     - Start the animation from the current cell
   stop()     - Stop the animation. Resets the current cell to the first frame
@@ -34,7 +34,7 @@ Functions:
 Usage:
   var playback = new PlayBack({
     imgSrc: "http://mysite.com/img/spritesheet.png",
-    target: "#playbackCanvas",
+    target: "playbackCanvas",
     blockWidth: 32,
     blockHeight 32,
     delay: 100
@@ -50,6 +50,8 @@ Usage:
   // Dev Note: we use the 'this' keyword so it attaches to the global context
   //           aka the 'window' object...
   this.PlayBack = function() {
+    var __class = this; // Self-reference for deeply nested functions
+
     var targetCanvas = null; // reference to the target canvas element
     var targetContext = null;
     var cellCount = 0;
@@ -69,6 +71,9 @@ Usage:
       delay: 1000.0 // 1s
     }
 
+    // event listeners
+    var listeners = [];
+
     // extend defaults with arguments to create final Options
     if(arguments[0] && typeof arguments[0] === "object") {
       this.options = extendDefaults(defaults, arguments[0]);
@@ -77,9 +82,21 @@ Usage:
     /********************************
     PRIVATE METHODS
     *********************************/
+    var triggerDrawEvent = () => {}; // Pre define
+    this.getFrame        = () => {}; // Pre define
+
     var fetchContext = function() {
       targetCanvas  = document.getElementById(defaults.target);
       targetContext = targetCanvas.getContext('2d');
+    }
+
+    var onImageLoad = function() {
+      // set canvas equal to spritesheet block size
+      targetCanvas.width  = defaults.blockWidth;
+      targetCanvas.height = defaults.blockHeight;
+
+      // 1st draw
+      redraw();
     }
 
     var loadImg = function() {
@@ -87,10 +104,10 @@ Usage:
       if(!defaults.imgPtr) {
         imgObj        = new Image;
         imgObj.src    = defaults.imgSrc;
-        imgObj.onload = redraw;
+        imgObj.onload = onImageLoad;
       } else {
         imgObj        = defaults.imgPtr;
-        redraw();
+        onImageLoad();
       }
 
       // Count total frames
@@ -102,7 +119,7 @@ Usage:
       var cols, row, col;
 
       // If frames have been supplied by the user, calculate the cell position
-      if(Array.isArray(defaults.frames)){
+      if(Array.isArray(defaults.frames) && defaults.frames.length > 0){
         var index = defaults.frames[currFrame]; // Get the cell from frames
         cols = imgObj.width / defaults.blockWidth;
         row = Math.floor(index / cols);
@@ -121,28 +138,65 @@ Usage:
 
     var redraw = function() {
       // clear & redraw the color buffer for playback
-      targetContext.clearRect(0, 0, defaults.blockWidth, defaults.blockHeight);
+      targetContext.clearRect(0, 0, targetCanvas.width, targetCanvas.height);
 
       // First 4 argument define the size of the image to draw,
       // Last  4 define the rect part of the image to draw.
       var view = calcCellRect();
       targetContext.drawImage(imgObj,
         view.x, view.y, defaults.blockWidth, defaults.blockHeight,
-        0, 0, defaults.blockWidth, defaults.blockHeight);
+        0, 0, targetCanvas.width, targetCanvas.height);
+
+      triggerDrawEvent();
     }
 
-    var step = function() {
+    var step = function(amt) {
       // If frames have been supplied by the user, flip through those cells only
-      if(Array.isArray(defaults.frames)) {
-        currFrame++;
+      if(Array.isArray(defaults.frames) && defaults.frames.length > 0) {
+        if(amt) {
+          currFrame += amt;
+          if(currFrame < 0) { currFrame = defaults.frames.length + currFrame; }
+        } else {
+          currFrame++;
+        }
+
         currFrame = currFrame % defaults.frames.length;
       } else {
         // Otherwise, loop through every frame
-        currCell++;
+        if(amt) {
+          currCell += amt;
+          if(currCell < 0) { currCell = cellCount + currCell; }
+        } else {
+          currCell++;
+        }
+
         currCell = currCell % cellCount;
       }
 
       redraw();
+    }
+
+    // implement
+    triggerDrawEvent = function() {
+      if(window.jQuery) {
+        // jQuery is loaded
+        $(targetCanvas).trigger({
+          type: "draw",
+          frame: __class.getFrame(),
+          time: new Date()
+        });
+
+        //console.log("jquery event fired");
+      } else {
+        // jQuery is not loaded, use the provided listener-subscriber utility
+        for(var i = 0; i < listeners.length; i++) {
+          listeners[i](__class.getFrame());
+        }
+
+        //console.log("listener event fired")
+      }
+
+      //console.log("draw event fired from class");
     }
 
     /********************************
@@ -157,12 +211,62 @@ Usage:
       defaults.delay = msDelay
     }
 
-    this.getCurrentCell = function() {
-      return currCell;
+    // If using frames, return the frame index currFrame
+    // If frames have not been supplied, return null
+    this.getFrame = function() {
+      if(Array.isArray(defaults.frames) && defaults.frames.length > 0) {
+        if(currFrame < 0) {
+          currFrame = 0;
+        }
+
+        return currFrame;
+      }
+
+      return null;
+    }
+
+    // If using frames, return null
+    // If frames have not been supplied, return the cell index currCell
+    this.getCell = function () {
+      if(!Array.isArray(defaults.frames) || !(defaults.frames.length > 0)) {
+        return currCell;
+      }
+
+      return null;
     }
 
     this.getCellCount = function() {
       return cells.length;
+    }
+
+    this.getFrameCount = function() {
+      if(Array.isArray(defaults.frames) && defaults.frames.length > 0) {
+        return defaults.frames.length;
+      }
+
+      // Otherwise return -1
+      return -1;
+    }
+
+    this.getRows = function() {
+      return (imgObj.height / defaults.blockHeight) | 0;
+    }
+
+    this.getColumns = function() {
+      return (imgObj.width / defaults.blockWidth) | 0;
+    }
+
+    this.setFrames = function(frames) {
+      defaults.frames = frames;
+
+      // if the current frame in the playback is missing, assume the last frame
+      if(currFrame > defaults.frames.length - 1) {
+        currFrame = defaults.frames.length - 1;
+      }
+    }
+
+    this.clearFrames = function() {
+      defaults.frames = null;
     }
 
     this.play = function() {
@@ -179,6 +283,70 @@ Usage:
       clearInterval(playBackTimer);
     }
 
+    this.stepForward = function() {
+      // clear the step counter and halt animation
+      if(playBackTimer) {
+        this.pause();
+      }
+
+      step(); // increment frame
+    }
+
+    this.stepBackward = function() {
+      // clear the step counter and halt animation
+      if(playBackTimer) {
+        this.pause();
+      }
+
+      // force the next frame to count backward
+      step(-1);
+    }
+
+    this.refresh = function() {
+      redraw();
+    }
+
+    /***********************************************************
+    PUBLIC UTILITY FUNCTIONS
+    ***********************************************************/
+
+    this.utils = {
+      calcFrameFromPos: function(x, y, scale=1.0) {
+        var frame = -1, row = 0, col = 0;
+
+        var row = (y/(defaults.blockHeight*scale)) | 0;
+        var col = (x/(defaults.blockWidth*scale))  | 0;
+        var numCols = (imgObj.width / defaults.blockWidth) | 0;
+
+        frame = col + (row*numCols);
+
+        return frame;
+      },
+
+      calcCellRectFromPos: function(x, y, scale=1.0) {
+          var view = {x: 0, y: 0};
+          var w = defaults.blockWidth;
+          var h = defaults.blockHeight;
+
+          if(w != 0) {
+            view.x = (x/(w*scale)) | 0;
+            view.x *= w*scale;
+          }
+
+          if(h != 0) {
+            view.y = (y/(h*scale)) | 0;
+            view.y *= h*scale;
+          }
+
+          return view;
+      },
+
+      // callback functions have the signature (frameIndex) => { }
+      subscribeToDrawEvent: function(callback) {
+        listeners.push(callback);
+      }
+    };
+
     /*********************************
     RETURN READY OBJECT
     **********************************/
@@ -190,7 +358,7 @@ Usage:
   }
 
   /*********************************
-  UTILITY
+  CLASS AUX FUNCTIONS
   *********************************/
   function extendDefaults(source, properties) {
     var property;
